f22e3dd2eca844c0a6c9327ded98e8ffb76ef33e
Harcozy <emii1990@yahoo.com>
2024-02-22 14:22:38 +1100

    New

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,20 @@
-package com.example.myapplication
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // Make sure you have a layout file named activity_main.xml
+        setContentView(R.layout.activity_start)
+
+        val startButton: ImageButton = findViewById(R.id.imageButton)
+        startButton.setOnClickListener {
+            // Navigate to the Main Screen Activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
