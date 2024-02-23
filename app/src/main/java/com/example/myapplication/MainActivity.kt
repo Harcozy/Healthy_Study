54fbacf9a9ef174aa0f91d7801fc9b2ee053dd9d
Harcozy <emii1990@yahoo.com>
2024-02-23 13:18:45 +1100

    New

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,9 @@
+package com.example.myapplication
+
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
